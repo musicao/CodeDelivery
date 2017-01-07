613d2451b4491613b98f03df45a2ea4c4f9d1d92
israel <israeleduardo@gmail.com>
2017-01-07 14:42:37 -0200

    arquivos corrigidos para exibir nome

--- app/Transformers/ClientTransformer.php
+++ app/Transformers/ClientTransformer.php
@@ -20,22 +20,15 @@ class ClientTransformer extends TransformerAbstract
      */
     public function transform(Client $model)
     {
-        return [
-            'id'           => (int) $model->id,
-            'name'         =>       $model->name,
-            'email'        =>       $model->email,
-            'phone'        =>       $model->phone,
-            'address'      => (int) $model->address,
-            'zipcode'      =>       $model->zipcode,
-            'city'         =>       $model->city,
-            'state'        =>       $model->state,
-			
-
-            /* place your other model properties here */
-
-            'created_at' => $model->created_at,
-            'updated_at' => $model->updated_at
-        ];
+		return [
+			'name' => $model->user->name,
+			'email' => $model->user->email,
+			'phone' => $model->phone,
+			'address' => $model->address,
+			'zipcode' => $model->zipcode,
+			'city' => $model->city,
+			'state' => $model->state,
+		];
     }
 }
 
